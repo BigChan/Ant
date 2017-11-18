04d50d25f81e6043a557e2189c982242a7112d83
hehuacheng <675808336@qq.com>
2017-11-18 16:28:45 +0800

    登录注册功能
    登录注册功能

--- web/js/login.js
+++ web/js/login.js
@@ -3,27 +3,32 @@ function check_login(){
     var name=$("#user_name").val();
     var pass=$("#password").val();
     if(/^1[34578]\d{9}$/.test(name)&& /^.{6,10}$/.test(pass)){ 
-        if(name=="18813291630" && pass=="123456"){
-            alert("登录成功！");
-            $("#user_name").val("");
-            $("#password").val("");
-             window.location.assign("index.html")
-           }
-        // $.ajax({
-        //     url:"",
-        //     data:{"name":name,"pass":pass},
-        //     success:function(data){}
-        // })
-       
+        $.ajax({
+            url:"http://192.168.155.4:8080/login",        
+            data:{"username":name,"password":pass},
+            error:function(XMLHttpRequest, textStatus, errorThrown){
+              alert('登录失败'+XMLHttpRequest.status+textStatus)
+            },
+            success:function(data){
+               alert(data.message);
+               if(data.code == '1'){
+                $("#user_name").val("");
+                $("#password").val("");       
+                document.cookie = name;
+                 window.location.assign("index.html")
+               }
+            }
+        })
+       return
     } 
     else{
         $("#login_form").removeClass('shake_effect');  
         setTimeout(function()
         {
          $("#login_form").addClass('shake_effect')
-        },1);  
+        },1);
+        alert('账号或密码不正确')  
     }
-    
    }
    function check_register(){
      var name = $("#r_user_name").val();
@@ -35,21 +40,28 @@ function check_login(){
      var email_check = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(email);
      if(name_check && email_check &&pass_check &&(pass === repass) )
       {
-    //     $.ajax({
-    //     url:"",
-    //     data:{"name":name,"pass":pass,"email":email,"repass":repass},
-    //     success:function(data){}
-    //    })
-       alert("注册成功！");
-       $("#r_user_name").val("");
-       $("#r_password").val("");
-       $("#cr_password").val("");
-        $("#r_email").val("");
-       $('form').animate({
-        height: 'toggle',
-        opacity: 'toggle'
-    }, 'slow');
-    document.title = document.title =='码蚁——登录'?'码蚁——注册':'码蚁——登录';
+        $.ajax({
+        url:"http://192.168.155.4:8080/register",
+        data:{"username":name,"password":pass,"email":email},
+        error:function(XMLHttpRequest, textStatus, errorThrown){
+          alert('注册失败'+XMLHttpRequest.status+textStatus)
+        },
+        success:function(data){
+          alert(data.message)
+          if(data.code == '1'){
+            $("#r_user_name").val("");
+            $("#r_password").val("");
+            $("#cr_password").val("");
+             $("#r_email").val("");
+            $('form').animate({
+             height: 'toggle',
+             opacity: 'toggle'
+         }, 'slow');
+         document.title = document.title =='码蚁——登录'?'码蚁——注册':'码蚁——登录';
+          }
+        }
+       })
+      
       }
       else
       {
