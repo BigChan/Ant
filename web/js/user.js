4c9a958236631b4a49f1772366f6d975f7813374
hehuacheng <675808336@qq.com>
2017-11-18 14:44:25 +0800

    用户中心修改
    用户中心

--- web/js/user.js
+++ web/js/user.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 
 $('.left_part li').click(function(){
     var index = $('.left_part li').index($(this));
-    console.log(index)
     $(this).addClass('active')
     $(this).siblings().removeClass('active')
     $('.right_part table').eq(index).addClass('active')
