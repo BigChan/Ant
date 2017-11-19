9a6d36f46be51411c9d8c9118e8ca5da1ad6ba53
hehuacheng <675808336@qq.com>
2017-11-19 19:49:06 +0800

    用户中心头像修改
    用户中心头像修改

--- web/js/user.js
+++ web/js/user.js
@@ -1,11 +1,11 @@
 
-$(document).ready(function(){
-    $.ajax({
-        url:"192.168.191.1:8080/",
-        data:{"name":name,"pass":pass},
-        success:function(data){}
-    })
-})
+// $(document).ready(function(){
+//     $.ajax({
+//         url:"192.168.191.1:8080/",
+//         data:{"name":name,"pass":pass},
+//         success:function(data){}
+//     })
+// })
 
 $('.left_part li').click(function(){
     var index = $('.left_part li').index($(this));
@@ -14,3 +14,17 @@ $('.left_part li').click(function(){
     $('.right_part table').eq(index).addClass('active')
     $('.right_part table').eq(index).siblings().removeClass('active')
 })
+
+$('#img_file').change(function(){
+    loadImg();
+ })
+ function loadImg(){  
+var file = $("#img_file")[0].files[0];
+var reader = new FileReader();  
+var imgFile;  
+reader.onload=function(e) {  
+    imgFile = e.target.result;  
+    $("#image").attr('src', imgFile);  
+};   
+reader.readAsDataURL(file);
+}  
